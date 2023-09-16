9720ebc9a3f5c7fd7858a789d7fdf46a7757dc76
jmang11 <mangiamel@hartford.edu>
2023-09-15 23:07:24 -0400

    Creation of JM

--- Jeremys_Assignment1.java
+++ Jeremys_Assignment1.java
@@ -1,8 +1,17 @@
 // Import libraries here
 // Jeremy Mangiameli
 
-public class Assignment1 {
+public class Jeremys_Assignment1 {
   public static void main(String[] args) {
-    // Add your code here
+    System.out.println(x:'      JJJJJJJJJJJJJJJJJ MMMMMMM           MMMMMMM')
+    System.out.println(x:'      JJJJJJJJJJJJJJJJJ MMMMMMMM         MMMMMMMM')
+    System.out.println(x:'            JJJJJ       MMMM MMMM       MMMM MMMM')
+    System.out.println(x:'            JJJJJ       MMMM  MMMM     MMMM  MMMM')
+    System.out.println(x:'            JJJJJ       MMMM   MMMM   MMMM   MMMM')
+    System.out.println(x:'            JJJJJ       MMMM    MMMM MMMM    MMMM')
+    System.out.println(x:'   JJJJ     JJJJJ       MMMM     MMMMMMM     MMMM')
+    System.out.println(x:'   JJJJ     JJJJJ       MMMM      MMMMM      MMMM')
+    System.out.println(x:'    JJJJ   JJJJJ        MMMM       MMM       MMMM')
+    System.out.println(x:'      JJJJJJJJ          MMMM                 MMMM')
   }
 }
