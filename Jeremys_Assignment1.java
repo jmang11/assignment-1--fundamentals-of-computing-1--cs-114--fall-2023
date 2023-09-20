488920d8714ffd46c14f3b4aea743d5be0d3ac2f
jmang11 <mangiamel@hartford.edu>
2023-09-20 08:21:18 -0400

    Old JM

--- Jeremys_Assignment1.java
+++ Jeremys_Assignment1.java
@@ -3,16 +3,6 @@
 
 public class Jeremys_Assignment1 {
   public static void main(String[] args) {
-    System.out.println(x:'      JJJJJJJJJJJJJJJJJ MMMMMMM           MMMMMMM')
-    System.out.println(x:'      JJJJJJJJJJJJJJJJJ MMMMMMMM         MMMMMMMM')
-    System.out.println(x:'            JJJJJ       MMMM MMMM       MMMM MMMM')
-    System.out.println(x:'            JJJJJ       MMMM  MMMM     MMMM  MMMM')
-    System.out.println(x:'            JJJJJ       MMMM   MMMM   MMMM   MMMM')
-    System.out.println(x:'            JJJJJ       MMMM    MMMM MMMM    MMMM')
-    System.out.println(x:'   JJJJ     JJJJJ       MMMM     MMMMMMM     MMMM')
-    System.out.println(x:'   JJJJ     JJJJJ       MMMM      MMMMM      MMMM')
-    System.out.println(x:'    JJJJ   JJJJJ        MMMM       MMM       MMMM')
-    System.out.println(x:'      JJJJJJJJ          MMMM                 MMMM')
     System.out.println(x:'      JJJJJJJJJJJJJJJJJ MMMMMMM           MMMMMMM');
     System.out.println(x:'      JJJJJJJJJJJJJJJJJ MMMMMMMM         MMMMMMMM');
     System.out.println(x:'            JJJJJ       MMMM MMMM       MMMM MMMM');
@@ -33,7 +23,6 @@ public class Jeremys_Assignment1 {
 
     System.println(x:"Enter a degree in Fahrenheit:");
     userFahrenheit = scan.nextInt();
-    calcCelcius = (userFahrenheit-32)*(5/9)
     calcCelsius = (userFahrenheit-32)*(5/9)
 
     System.println(x:"Enter a String of 5-Characters:");
