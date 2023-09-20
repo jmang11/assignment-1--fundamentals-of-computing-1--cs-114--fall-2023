44c1fb32fb258af10c3c048a77d94f5456de4319
jmang11 <mangiamel@hartford.edu>
2023-09-20 08:04:46 -0400

    Add Semi-Colon for JM

--- Jeremys_Assignment1.java
+++ Jeremys_Assignment1.java
@@ -13,5 +13,15 @@ public class Jeremys_Assignment1 {
     System.out.println(x:'   JJJJ     JJJJJ       MMMM      MMMMM      MMMM')
     System.out.println(x:'    JJJJ   JJJJJ        MMMM       MMM       MMMM')
     System.out.println(x:'      JJJJJJJJ          MMMM                 MMMM')
+    System.out.println(x:'      JJJJJJJJJJJJJJJJJ MMMMMMM           MMMMMMM');
+    System.out.println(x:'      JJJJJJJJJJJJJJJJJ MMMMMMMM         MMMMMMMM');
+    System.out.println(x:'            JJJJJ       MMMM MMMM       MMMM MMMM');
+    System.out.println(x:'            JJJJJ       MMMM  MMMM     MMMM  MMMM');
+    System.out.println(x:'            JJJJJ       MMMM   MMMM   MMMM   MMMM');
+    System.out.println(x:'            JJJJJ       MMMM    MMMM MMMM    MMMM');
+    System.out.println(x:'   JJJJ     JJJJJ       MMMM     MMMMMMM     MMMM');
+    System.out.println(x:'   JJJJ     JJJJJ       MMMM      MMMMM      MMMM');
+    System.out.println(x:'    JJJJ   JJJJJ        MMMM       MMM       MMMM');
+    System.out.println(x:'      JJJJJJJJ          MMMM                 MMMM');
   }
 }
